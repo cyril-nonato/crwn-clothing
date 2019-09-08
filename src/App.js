8d649b9c56db157f4d906d05f8e56dc68908cca4
Cyril Jessie Nonato <03cyril.nonato@gmail.com>
2019-09-08 14:13:09 +0800

    update menu items with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
 
+const Hats = () => {
+  return (
+    <div>
+      Hats Page
+    </div>
+  )
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
